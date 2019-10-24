df87f120e49a90495b804fad00c3c28db5ca6d35
bangzhen fan <719577252@qq.com>
2019-10-24 19:06:07 +0800

    ori

---
+++ WebApplication1/WebApplication1/Controllers/UserController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class UsersController : ApiController
+    {
+        private User[] users = new User[] { new User{ id = 1, name = "Haleemah Redfern", email = "email1@mail.com", phone = "01111111", role = 1 },
+            new User { id = 2, name = "Aya Bostock", email = "email2@mail.com", phone = "01111111", role = 1} };
+    public IEnumerable<User> Get()
+        {
+            return users;
+        }
+
+        // GET: api/User/5
+        public User Get(int id)
+        {
+            
+            foreach(User u in users)
+            {
+                if (u.id == id)
+                {
+                    return u;
+                }
+                
+            }
+            return new User();
+        }
+
+        // POST: api/User
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/User/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/User/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class User
+    {
+   
+
+        public int id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string phone{ get; set; }
+        public int role { get; set; }
+
+    }
+}
